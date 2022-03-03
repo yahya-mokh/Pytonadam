3d7fd847de2c192ed4675aa60d916376775bb38e
Yahya MOKHTARI <ymokhtari@hvsgrp.fr>
2022-03-03 23:07:19 +0100

    commit exercice diviseur nombre premier

---
+++ AppData/Local/Programs/Python/Python310/exercicenpremier.py
@@ -0,0 +1,23 @@
+import math
+def dviseurdeN():
+    listdediviseurdeN = []
+    print(listdediviseurdeN)
+    x =  int(input("entrer le nombre entier n : "))
+    for i in range(1, x+1):
+        a = (math.fmod(x,i))
+        if a == 0:
+            #print(int(i))
+            listdediviseurdeN.append(i)
+    print(listdediviseurdeN)
+    #if len(listdediviseurdeN)==2:
+    if listdediviseurdeN[1]==x:
+        print(x," : est un nombre premier")
+    else:
+         print(x," : n'est pas un nombre premier")
+           
+
+            
+    
+    
+    
+dviseurdeN()
